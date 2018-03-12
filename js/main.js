a223178b0dd93d12182e7794b91e0644202ab9e7
Gareth Jones <garethwyndhamjones@gmail.com>
2018-03-12 17:18:15 +1100

    Pop-Ups

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
-// notavailable
-// wip
+console.log("You must construct additional scripts");
+
+document.getElementById("notavailable").onclick = function() {
+  alertNa();
+};
+
+function alertNa() {
+  window.alert(
+    "Since Vertigo is a WP site owned by UTS, i unfortunately can't share the source code for this particular project!"
+  );
+}
+
+document.getElementById("wip").onclick = function() {
+  alertWip();
+};
+
+function alertWip() {
+  window.alert(
+    "This project is currently a work in progress! A demo site and documentation will be coming soon!"
+  );
+}
